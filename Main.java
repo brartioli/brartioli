3bf729a18c1966be9e56ac893d0aab1632ddfc6e
brartioli <brunaartioli.ba@gmail.com>
2024-08-17 12:34:46 -0300

    Add files via upload
    exercicio de Java estrutura repetitiva for

--- Main.java
+++ Main.java
@@ -1,35 +1,24 @@
-package exerciciosfor;
+package exercícios_for05;
 
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		int fato = 1;
 		
-			int N = sc.nextInt();
-			
-			for(int i = 0; i<N; i++)
-			{
-				double v1 = sc.nextDouble();
-				double v2 = sc.nextDouble();
-				
-				if (v2 == 0)
-				{
-					System.out.println("divisao impossivel");
-				}
-				else 
-				{
-					double div = (double) v1 / v2;
-					System.out.printf("%.1f%n", div);
-				}
-				
-			}
-			
+		for(int i = 1; i<=n; i++)
+		{
+			fato *= i;
+		}
+		System.out.println(fato);
+		
 		sc.close();
 	}
 
